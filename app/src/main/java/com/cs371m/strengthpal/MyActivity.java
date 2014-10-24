9e372c293277d4ee2e60b23acae642a0aab08f3a
tcorley <tyler_corley@me.com>
2014-10-23 22:35:14 -0500

    DON'T HURT ME IT DOESN'T WORK YET

--- app/src/main/java/com/cs371m/strengthpal/MyActivity.java
+++ app/src/main/java/com/cs371m/strengthpal/MyActivity.java
@@ -1,36 +1,122 @@
 package com.cs371m.strengthpal;
 
 import android.app.Activity;
+import android.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.view.Gravity;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.support.v4.widget.DrawerLayout;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
 public class MyActivity extends Activity {
+    // Variable for the drawer
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private CharSequence mDrawerTitle;
+    private CharSequence mTitle;
+    private String[] mDrawerTitleLabels;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+
+        // get this shit ready
+        mTitle = mDrawerTitle = getTitle();
+        mDrawerTitleLabels = getResources().getStringArray(R.array.drawer_options);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+
+        // fancy ass shadow
+        mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow, Gravity.START);
+        // drawer's list view and a click listener
+        // because each title is going to be different, we might need to rethink this line
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this, R.layout.drawer_list_item, mDrawerTitleLabels));
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+
+        // make the icon on the action bar behave as a toggle for the drawer
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setHomeButtonEnabled(true);
+
+        // app bar icon, button stuff to make sure it works together
+        mDrawerToggle = new ActionBarDrawerToggle(
+                this,
+                mDrawerLayout,
+                R.drawable.ic_drawer,
+                R.string.drawer_open,
+                R.string.drawer_close
+                ) {
+            public void onDrawerClosed(View drawerView) {
+                getActionBar().setTitle(mTitle);
+                invalidateOptionsMenu();
+            }
+
+            public void onDrawerOpened(View drawerView) {
+                getActionBar().setTitle(mDrawerTitle);
+                invalidateOptionsMenu();
+            }
+        };
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+
+        if (savedInstanceState == null) {
+            selectItem(0);
+        }
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.my, menu);
-        return true;
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.my, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    /* Called when we call invalidateOptionsMenu() */
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        // if the drawer is open, hide action items related to content view
+        boolean drawerOpen = mDrawerLayout.isDrawerOpen(mDrawerList);
+        //follow this pattern. we won't have a websearch but we might have options pertaining to
+        //each fragment. maybe a switch() here?
+        //menu.findItem(R.id.action_websearch).setVisible(!drawerOpen);
+        return super.onPrepareOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+        // The action bar home/up action should open/close the drawer. ActionBarDrawerToggle does it.
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
         int id = item.getItemId();
+        //handle different things here
         if (id == R.id.action_settings) {
             return true;
         }
         return super.onOptionsItemSelected(item);
+
+    }
+
+    /* Implement the click listener for the listview in the drawer */
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            selectItem(position);
+        }
+    }
+
+    private void selectItem(int position) {
+        //update main content with a new fragment
+        Fragment fragment = new Fragment();
     }
 }
