08019a29c0931cf389a0c4b5691d5873f094a333
ehvatum <ehvatum@utexas.edu>
2014-10-26 21:04:22 -0500

    Search ranked by distance and shows place names

--- app/src/main/java/com/cs371m/strengthpal/GymFragment.java
+++ app/src/main/java/com/cs371m/strengthpal/GymFragment.java
@@ -4,17 +4,25 @@ import android.app.Fragment;
 import android.content.Context;
 import android.location.Location;
 import android.location.LocationManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.model.LatLng;
-
+import com.google.android.gms.maps.model.MarkerOptions;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class GymFragment extends Fragment {
 
@@ -23,12 +31,13 @@ public class GymFragment extends Fragment {
     LocationManager locationManager;
     List<String> networkProviders;
     Location currentLocation;
+    GoogleMap map;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_gym, container, false);
 
-        GoogleMap map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+        map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
         map.setMyLocationEnabled(true);
 
         locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
@@ -44,8 +53,87 @@ public class GymFragment extends Fragment {
         if (currentLocation != null) {
             LatLng coordinates = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
             map.moveCamera(CameraUpdateFactory.newLatLngZoom(coordinates, 13));
+
+            String query = "https://maps.googleapis.com/maps/api/place/search/json" +
+                    "?location=" + currentLocation.getLatitude() + "," + currentLocation.getLongitude() +
+                    "&rankby=distance" +
+                    "&types=gym" +
+                    "&key=AIzaSyAgAA0SJSFBydGuJpbJ8LlnUPoQx9CM4PU";
+
+            getJSON jsonGetter = new getJSON();
+            jsonGetter.execute(query);
+
+            try {
+                jsonGetter.get(1000000, TimeUnit.MILLISECONDS);
+            }
+            catch (Exception e) {
+                // do nothing
+            }
+
+            for (int i = 0; i < 25; i++) {
+                if (jsonGetter.coordinates[i][0] != 0) {
+                    map.addMarker(new MarkerOptions().position(new LatLng(jsonGetter.coordinates[i][0], jsonGetter.coordinates[i][1])).title(jsonGetter.names[i]));
+                }
+            }
         }
 
         return rootView;
+    }
+
+    private class getJSON extends AsyncTask<String, Integer, String> {
+
+        double[][] coordinates  = new double[25][2];
+        String[] names = new String[25];
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            String strJson = null;
+            try {
+                DefaultHttpClient defaultHttpClient = new DefaultHttpClient();
+                HttpPost httpPost = new HttpPost(params[0]);
+                HttpResponse httpResponse = defaultHttpClient.execute(httpPost);
+                HttpEntity httpEntity = httpResponse.getEntity();
+                strJson = EntityUtils.toString(httpEntity);
+            }
+            catch (Exception e) {
+                // do nothing
+            }
+
+
+            JSONObject json = null;
+            try {
+                json = new JSONObject(strJson);
+            }
+            catch (Exception e) {
+                // do nothing
+            }
+
+            JSONArray results = null;
+            if (json != null) {
+                try {
+                    results = json.getJSONArray("results");
+                }
+                catch (Exception e) {
+                    // do nothing
+                }
+            }
+
+            if (results != null) {
+                for (int i = 0; i < results.length(); i++) {
+                    try {
+                        JSONObject item = results.getJSONObject(i);
+                        coordinates[i][0] = item.getJSONObject("geometry").getJSONObject("location").getDouble("lat");
+                        coordinates[i][1] = item.getJSONObject("geometry").getJSONObject("location").getDouble("lng");
+                        names[i] = item.getString("name");
+                        if (i == 24)
+                            break;
+                    } catch (Exception e) {
+                        // do nothing
+                    }
+                }
+            }
+            return "success";
+        }
     }
 }
