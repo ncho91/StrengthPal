877925a874805f2ab5b56487345cb7d6f0f3bc6e
ncho91 <noah_cho@utexas.edu>
2014-10-26 21:14:09 -0500

    workouts have buttons in them for programs. made "new workout" have the ability to add a row with empty EditTexts so that a user can add exercises

--- app/src/main/java/com/cs371m/strengthpal/HistoryFragment.java
+++ app/src/main/java/com/cs371m/strengthpal/HistoryFragment.java
@@ -1,11 +1,12 @@
 package com.cs371m.strengthpal;
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * Created by Tyler_iMac on 10/23/14.
@@ -17,6 +18,14 @@ public class HistoryFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_history, container, false);
+
+        final Button newWorkoutButton = (Button) rootView.findViewById(R.id.new_workout_button);
+        newWorkoutButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent myIntent = new Intent(v.getContext(), NewWorkoutActivity.class);
+                startActivityForResult(myIntent, 0);
+            }
+        });
 
         return rootView;
     }
--- app/src/main/java/com/cs371m/strengthpal/WorkoutFragment.java
+++ app/src/main/java/com/cs371m/strengthpal/WorkoutFragment.java
@@ -2,10 +2,10 @@ package com.cs371m.strengthpal;
 
 import android.app.Fragment;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * Created by Tyler_iMac on 10/23/14.
@@ -17,6 +17,38 @@ public class WorkoutFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_workout, container, false);
+
+
+        /*
+            fill out workout entries with default values as per program specification
+         */
+        final Button startingStrengthbutton = (Button) rootView.findViewById(R.id.starting_strength_button);
+        startingStrengthbutton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+            }
+        });
+
+        final Button strongLiftbutton = (Button) rootView.findViewById(R.id.strong_lifts_button);
+        strongLiftbutton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+            }
+        });
+
+        final Button wendlerButton = (Button) rootView.findViewById(R.id.wendler_button);
+        wendlerButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+            }
+        });
+
+        final Button phatButton = (Button) rootView.findViewById(R.id.phat_button);
+        phatButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+            }
+        });
 
         return rootView;
     }
